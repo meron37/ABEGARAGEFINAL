ae8c46a1db952a897fa80916f90da639a79b5567
meron37 <134938986+meron37@users.noreply.github.com>
2024-04-13 17:48:00 -0500

    Implemented the employee pages

--- backend/controllers/employee.controller.js
+++ backend/controllers/employee.controller.js
@@ -1,13 +1,15 @@
-// Import the employee service 
-const employeeService = require('../services/employee.service');
+// Import the employee service
+const employeeService = require("../services/employee.service");
 // Create the add employee controller
 async function createEmployee(req, res, next) {
-  // Check if employee email already exists in the database 
-  const employeeExists = await employeeService.checkIfEmployeeExists(req.body.employee_email);
+  // Check if employee email already exists in the database
+  const employeeExists = await employeeService.checkIfEmployeeExists(
+    req.body.employee_email
+  );
   // If employee exists, send a response to the client
   if (employeeExists) {
     res.status(400).json({
-      error: "This email address is already associated with another employee!"
+      error: "This email address is already associated with another employee!",
     });
   } else {
     try {
@@ -16,7 +18,7 @@ async function createEmployee(req, res, next) {
       const employee = await employeeService.createEmployee(employeeData);
       if (!employee) {
         res.status(400).json({
-          error: "Failed to add the employee!"
+          error: "Failed to add the employee!",
         });
       } else {
         res.status(200).json({
@@ -24,14 +26,32 @@ async function createEmployee(req, res, next) {
         });
       }
     } catch (error) {
-      console.log(err);
+      console.log(error);
       res.status(400).json({
-        error: "Something went wrong!"
+        error: "Something went wrong!",
       });
     }
   }
 }
-// Export the createEmployee controller 
+
+// Create the getAllEmployees controller
+async function getAllEmployees(req, res, next) {
+  // Call the getAllEmployees method from the employee service
+  const employees = await employeeService.getAllEmployees();
+  // console.log(employees);
+  if (!employees) {
+    res.status(400).json({
+      error: "Failed to get all employees!",
+    });
+  } else {
+    res.status(200).json({
+      status: "success",
+      data: employees,
+    });
+  }
+}
+// Export the createEmployee controller
 module.exports = {
-  createEmployee
+  createEmployee,
+  getAllEmployees,
 };
